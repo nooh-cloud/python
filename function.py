3afc05789e12f4033fbfb697808ddcffa28bcf20
nooh-cloud <abdullanooh552@gmail.com>
2024-10-28 12:46:10 +0530

    nein

---
+++ function.py
@@ -0,0 +1,13 @@
+def my_function(num1,num2):
+    add = num1 + num2
+    print("after adding: ",add)
+    sub = num1 - num2
+    print("after substracting: ",sub)
+    multiply = num1 * num2
+    print("after multiply: ",multiply)
+    division = num1 / num2
+    print("after division: ",division)
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+my_function(a,b)
