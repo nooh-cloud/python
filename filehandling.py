0d073834218165057e34c2063f03aa83e276d21f
nooh-cloud <abdullanooh552@gmail.com>
2024-11-07 11:11:34 +0530

    file

---
+++ filehandling.py
@@ -0,0 +1,44 @@
+# q1
+# # f = open("myfile.txt","x")
+
+# # f = open("myfile.txt", "a")
+# # f.write("HELLO WORLD!")
+# # f.close()
+
+# # f = open("myfile.txt", "r")
+# # print(f.read())
+
+
+
+# q2
+# f = open("sample.txt", "r")
+# print(f.read())
+
+
+
+# q3
+# f = open("sample.txt", "a")
+# f.write("This is an additional line")
+# f.close()
+
+# f = open("sample.txt", "r")
+# print(f.read())
+
+
+
+# q4
+# with open("sample.txt", "r") as file:
+#     line_count = sum(1 for line in file)
+#     print(f"Number of lines: {line_count}")
+
+
+
+
+# q5
+# with open("sample.txt", "r") as file:
+#     words = file.read().split()
+#     word_count = len(words)
+#     print(f"Number of words: {word_count}")
+
+
+
